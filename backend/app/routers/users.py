17f1819fe1d3c89b17ddeab48b77e984a9534374
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2025-09-04 20:46:10 -0300

    feat: delete old fake db

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -27,19 +27,6 @@ from app.db.fake_db import fake_db
 
 router = APIRouter()
 
-fake_db = [
-  {"id": 1, "name": "Alice"},
-  {"id": 2, "name": "Bob"},
-  {"id": 3, "name": "Carol"},
-  {"id": 4, "name": "David"},
-  {"id": 5, "name": "Eve"},
-  {"id": 6, "name": "Frank"},
-  {"id": 7, "name": "Grace"},
-  {"id": 8, "name": "Heidi"},
-  {"id": 9, "name": "Ivan"},
-  {"id": 10, "name": "Judy"}
-]
-
 @router.get("/")
 def get_users():
     return fake_db
