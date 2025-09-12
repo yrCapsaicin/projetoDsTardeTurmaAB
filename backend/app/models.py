19651d78f31b99542dad79a8dde0e6a13b47a097
Gustavo Martins de Carvalho Santos Ribeiro <gustavo.ribeiro143@etec.sp.gov.br>
2025-09-11 21:09:18 -0300

    Complementando o Modelo da Classe User
    adicionei alguns campos

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
 
 class User(BaseModel):
+    id: int
     name: str
+    email: str
+    password: float
+    bio: str
+    styles: list[str]
 
 class Music(BaseModel):
     title: str
