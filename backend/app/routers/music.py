f94db02bf7dba63c51483c0486a677c541244d5f
Vinicius Santos da Silva <jorgevinny2008@gmail.com>
2025-09-18 19:10:10 -0300

    added the GET music routers

--- backend/app/routers/music.py
+++ backend/app/routers/music.py
@@ -1,4 +1,15 @@
 from fastapi import APIRouter
-from app.models import User
+from app.models import Music
 from app.db.fake_db import fake_db
 
+router = APIRouter()
+
+@router.get("/")
+def get_music():
+    return fake_db.musics
+
+@router.get("/{music_id}")
+def get_music(music_id: int):
+    music = next((music for music in fake_db if music["id"] == music_id), None)
+    if music:
+        return music
