e0428b472ebc46bd17f4b7db2a167d40609fcdf9
Vinnyforce0 <jorgevinny2008@gmail.com>
2025-09-19 15:18:07 -0300

    adicionei o router POST para a postagem de musicas no banco

--- backend/app/routers/music.py
+++ backend/app/routers/music.py
@@ -13,3 +13,9 @@ def get_music(music_id: int):
     music = next((music for music in fake_db if music["id"] == music_id), None)
     if music:
         return music
+
+@router.post("/")
+def create_music(music: Music):
+    new_music = {"id": len(fake_db) + 1, "name": music.name}
+    fake_db.music.append(new_music)
+    return new_music
