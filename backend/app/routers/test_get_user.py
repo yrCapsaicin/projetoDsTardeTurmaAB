785a71b8801046cf643339dd91281f6919a2e880
Lucas Goulart <lucasggoulart2505@gmail.com>
2025-09-20 23:54:25 -0300

    Verificações adicionais na resposta

--- backend/app/routers/test_get_user.py
+++ backend/app/routers/test_get_user.py
@@ -4,7 +4,7 @@ from app.main import app
 client = TestClient(app)
 
 def test_get_user():
-    user = {"name": "João"}
+    user = {"name": "Joï¿½o"}
     create_response = client.post("/users/", json=user)
     created_user = create_response.json()
 
@@ -12,4 +12,8 @@ def test_get_user():
     response = client.get(f"/users/{user_id}")
     assert response.status_code == 200
     assert response.json()["id"] == user_id
-    assert response.json()["name"] == "João"
+    assert response.json()["name"] == "Joï¿½o"
+
+    data = response.json()
+    assert isinstance(data, dict)
+    assert set(data.keys()) >= {"id", "name"}
