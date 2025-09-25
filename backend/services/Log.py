9177b24699585644e1dba139cf96dc8f9a99ecf3
Felipe Cesare <felipegithub18@gmail.com>
2025-09-25 15:11:37 -0300

    recriei o EventoBack.log e arrumei o nome no filename
    recriei o EventoBack.log e arrumei o nome no filename. pq tinham apagado o evento.log

--- backend/services/Log.py
+++ backend/services/Log.py
@@ -1,5 +1,5 @@
 import logging
-logging.basicConfig(format='%(asctime)s %(message)s', filename='eventos.log', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(format='%(asctime)s %(message)s', filename='EventosBack.log', datefmt='%m/%d/%Y %I:%M:%S %p')
 logging.warning('foi quando este evento foi registrado.')   
 
 
