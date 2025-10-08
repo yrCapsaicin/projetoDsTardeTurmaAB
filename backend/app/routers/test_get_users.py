c2f65898b4fff0aa919b7d02a5a38fac480807ca
Goulw <lucasggoulart2505@gmail.com>
2025-10-07 22:17:01 -0300

    Fazendo a condição agora no test_get_users.py

--- backend/app/routers/test_get_users.py
+++ backend/app/routers/test_get_users.py
@@ -10,10 +10,18 @@ def test_get_users():
         
     user = {"name": "Maria"}
     create_response = client.post("/users/", json=user)
+    if create_response.status_code == 404:
+        print("Erro 404! Usuário não encontrado.")
+        print("Response content:", response.json())
+        return
     assert create_response.status_code == 201
     created_user = create_response.json()
 
     updated_response = client.get("/users/")
+    if updated_response.status_code == 404:
+        print("Erro 404! Usuário não encontrado.")
+        print("Response content:", response.json())
+        return
     assert updated_response.status_code == 200
     users = updated_response.json()
     assert isinstance(users, list)
