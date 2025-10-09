17c982074f63a21f7725c51428745ef132c0968e
Alisson Seabra <alisson.seabra@etec.sp.gov.br>
2025-10-09 15:37:33 -0300

    SelectGeneroMusic
    Resolve #289
    criei o esqueleto da página Seleção do Genero segundo o Figma
    (SEM ESTILIZAÇÃO)

---
+++ frontend/app/home.jsx
@@ -0,0 +1,134 @@
+import React, { useRef, useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  SafeAreaView,
+  Animated,
+  Button,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useRouter } from 'expo-router';
+
+const App = () => {
+  const rout = useRouter();
+  function fnSelect (){
+    rout.push('/selectGenero');
+  }
+  return (
+   
+    <LinearGradient
+    colors={['#962fbf', '#d62976', '#fa7e1e', '#feda75', '#4f5bd5']} 
+    style={styles.container}
+    start={{ x: 0.5, y: 0 }}    // topo centralizado (x = 0.5, y = 0)
+    end={{ x: 0.5, y: 1 }}     
+  >
+  
+
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Bem Vindo, "nome"!!</Text>
+        </View>
+        <View style={styles.fundoPost}>
+        <View style={styles.postagem}>
+        <Text style={styles.titulo}>Título da Postagem</Text>
+        <Text style={styles.conteudo}>
+          Este é o conteúdo da sua postagem. Você pode colocar texto, imagens ou qualquer outro componente aqui.
+        </Text>
+        </View>
+        <View style={styles.postagem}>
+        <Text style={styles.titulo}>Título da Postagem</Text>
+        <Text style={styles.conteudo}>
+          Este é o conteúdo da sua postagem. Você pode colocar texto, imagens ou qualquer outro componente aqui.
+        </Text>
+        </View>
+        <View style={styles.postagem}>
+        <Text style={styles.titulo}>Título da Postagem</Text>
+        <Text style={styles.conteudo}>
+          Este é o conteúdo da sua postagem. Você pode colocar texto, imagens ou qualquer outro componente aqui.
+        </Text>
+        </View>
+
+      </View>
+        <View style={styles.nav}>
+          <TouchableOpacity style={styles.navItem}>
+            <Text style={styles.navText1}>Notificação</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.navItem}>
+            <Text style={styles.navText1}>Player</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.navItem}>
+            <Text style={styles.navText1}>Home</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.navItem}>
+            <Text style={styles.navText1}>Chat</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.navItem}>
+            <Text style={styles.navText1}>Perfil</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  header: {
+    padding: 20,
+    alignItems: 'center',
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  fundoPost:{
+    height: 400,
+  },
+  postagem: {
+    backgroundColor: 'rgba(128, 128, 128, 0.45)',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 16,
+    width: '100%',
+    maxWidth: 400,
+    marginBottom: 10,
+  },
+  titulo: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff', // branco para contraste
+    marginBottom: 8,
+  },
+  conteudo: {
+    fontSize: 16,
+    color: '#f0f0f0',
+  },
+  nav: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#1d1436',
+    borderRadius: 10,
+    paddingVertical: 10,
+    marginBottom: 2,
+  },
+  navItem: {
+    padding: 10,
+  },
+  navText1: {
+    fontSize: 16,
+    color: '#ff3cf5',
+  },
+});
+
+export default App;
