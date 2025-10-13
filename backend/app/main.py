8443d153ce5d58fb89d93803cb0edc21098a38e7
Matheus Pascheto <matheusdapoia@gmail.com>
2025-10-12 23:56:45 -0300

    Importando supabase_client para o arquivo main.py (esqueci de fazer isso antes ja que eu nao usei o main para fazer os testes)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routers import users
-
+from app.db.supabase_client import get_supabase, SUPABASE_KEY, SUPABASE_URL
 app = FastAPI(title="Backend React Native API")
 
 # incluir rotas
